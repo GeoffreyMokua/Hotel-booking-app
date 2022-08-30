759ee7de864592542d64112033d66ec1602155f6
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-08-30 21:03:37 +0300

    this commit will add the first function which take inputs

--- main.js
+++ main.js
@@ -11,4 +11,17 @@ function slider() {
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
   setTimeout(slider, 10000); // Change image every 2 seconds
-}
+} 
+// js for the booking section
+;(function () {
+    'use strict';
+    $.fn.handleCounter = function (options) {
+        var $input,
+            $btnMinus,
+            $btnPlugs,
+            minimum,
+            maximize,
+            writable,
+            onChange,
+            onMinimum,
+            onMaximize;
